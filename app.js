51357b4ed46ab6dce5276743bd857f82db22da64
Oleksii Verbytskyi <alverba88@gmail.com>
2023-06-04 23:13:39 +0300

    fix typo error

--- app.js
+++ app.js
@@ -2,10 +2,9 @@ const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
 require("dotenv").config();
+
 const orderRouter = require("./src/routes/api/orders");
 const couponRouter = require("./src/routes/api/coupons");
-
-
 
 const app = express();
 
@@ -16,16 +15,16 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
 
-app.use('api/orders', orderRouter);
-app.use('api/coupons', couponRouter);
+app.use('/api/orders', orderRouter);
+app.use('/api/coupons', couponRouter);
 
 app.use((req, res) => {
-  res.status(404).json({ message: "Not found" });
+    res.status(404).json({message: "Not found"});
 });
 
 app.use((err, req, res, next) => {
-  const { status = 500, message = "Server error" } = err;
-  res.status(status).json({ message });
+    const {status = 500, message = "Server error"} = err;
+    res.status(status).json({message});
 });
 
 module.exports = app;
