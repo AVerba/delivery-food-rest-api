1a8d8bc703e6412a7ac9a83ccf2a58b75bc289ee
Oleksii Verbytskyi <alverba88@gmail.com>
2023-06-08 22:49:21 +0300

    mod coupon controller

--- src/controllers/coupons/createCoupon.js
+++ src/controllers/coupons/createCoupon.js
@@ -1,24 +1,17 @@
 const Coupon = require('../../models/coupon');
 
+
 const createCoupon = async (req, res) => {
+    const {_id: owner} = req.user;
+
     try {
-        // eslint-disable-next-line camelcase
-        const {code, discount_amount, expiration_date} = req.body;
+        const couponData = { ...req.body, owner };
+        const order = await Coupon.create(couponData);
 
-        // Check if the coupon code already exists
-        const existingCoupon = await Coupon.findOne({code});
-        if (existingCoupon) {
-            return res.status(400).json({error: 'Coupon code already exists'});
-        }
-
-        const coupon = new Coupon({code, discount_amount, expiration_date});
-        await coupon.save();
-
-        res.status(201).json({coupon});
+        res.status(201).json(order);
     } catch (error) {
         console.error(error);
         res.status(500).json({error: 'Internal server error'});
     }
 };
-
 module.exports = createCoupon;
