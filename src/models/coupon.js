a0c7c6eff40f8aa137452fa7fd4b439e7fa9c7c0
Oleksii Verbytskyi <alverba88@gmail.com>
2023-06-10 14:20:54 +0300

    mod coupons model

--- src/models/coupon.js
+++ src/models/coupon.js
@@ -1,10 +1,15 @@
 const {Schema, model} = require("mongoose");
+const Joi = require("joi");
 
 const couponSchema = new Schema({
     code: {
         type: String,
         required: true,
         unique: true
+    },
+    coupon_date: {
+        type: Date,
+        default: Date.now
     },
     discount_amount: {
         type: Number,
@@ -13,10 +18,24 @@ const couponSchema = new Schema({
     expiration_date: {
         type: Date,
         required: true
-    }
+    },
+    owner: {
+        type: Schema.Types.ObjectId,
+        ref: "user",
+        required: true,
+    },
 });
+
+const addCoupon = Joi.object({
+    code: Joi.string().required(),
+    coupon_date: Joi.date().default(Date.now),
+    discount_amount: Joi.number().required(),
+    expiration_date: Joi.date().required(),
+});
+
+
 const schemas = {
-    coupon: couponSchema,
+    coupon: addCoupon,
 };
 const Coupon = model('Coupon', couponSchema);
 
